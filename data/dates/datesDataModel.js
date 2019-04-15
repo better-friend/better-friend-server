a2ffc7691edd70b16f78d99d9ca051ae9682668e
Francis Chen <fncis.chen@gmail.com>
2019-04-15 10:47:18 -0700

    updated datesDataModel

--- data/dates/datesDataModel.js
+++ data/dates/datesDataModel.js
@@ -3,43 +3,38 @@ const db = require("../dbConfig.js");
 module.exports = {
     getAllDatesByUsername,
     getDatesById,
-    getDatesByEventType,
-    getDatesByRelationshipType,
     getDatesByPersonName,
     insertDate,
     updateDate,
     deleteDate
 }
 
-function getAllDatesByUsername(username){
-
+function getAllDatesByUsername(user_id){
+    return db("dates").where("user_id", user_id);
 }
 
-function getDatesById(username, id){
-
+function getDatesById(user_id, date_id){
+    return db("dates").where({"user_id": user_id, "date_id": date_id});
 }
 
-function getDatesByEventType(username, event) {
+function getDatesByPersonName(user_id, person) {
 
+    return db("dates").where({"user_id": username, "person": person});
 }
 
-function getDatesByRelationshipType(username, relationship) {
-
-}
-
-function getDatesByPersonName(username, person) {
-
-}
-
-function insertDate(username, dateData) {
-
-}
-
-function updateDate(username, id, dateData) {
+function insertDate(dateData) {
     
+    let user_id = db("users").where("user_id", dateData.username).first().user_id;
+    dateData.user_id = user_id; 
+
+    return db("dates").insert(dateData);
 }
 
-function deleteDate(username, id, dateData) {
-    
+function updateDate(user_id, date_id, dateData) {
+    return db("dates").where({"user_id": user_id, "date_id": date_id}).insert(dateData);
+}
+
+function deleteDate(user_id, id) {
+    return db("dates").where({"user_id": user_id, "id": id})
 }
 
