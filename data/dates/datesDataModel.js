77c6a467597d0b0a98344b7860de1bd61c9a4b96
Francis Chen <fncis.chen@gmail.com>
2019-04-14 22:26:23 -0700

    set up appropate user and dates data model

---
+++ data/dates/datesDataModel.js
@@ -0,0 +1,43 @@
+module.exports = {
+    getAllDatesByUsername,
+    getDatesById,
+    getDatesByEventType,
+    getDatesByRelationshipType,
+    getDatesByPersonName,
+    insertDate,
+    updateDate,
+    deleteDate
+}
+
+function getAllDatesByUsername(username){
+
+}
+
+function getDatesById(username, id){
+
+}
+
+function getDatesByEventType(username, event) {
+
+}
+
+function getDatesByRelationshipType(username, relationship) {
+
+}
+
+function getDatesByPersonName(username, person) {
+
+}
+
+function insertDate(username, dateData) {
+
+}
+
+function updateDate(username, dateData) {
+    
+}
+
+function deleteDate(username, dateData) {
+    
+}
+
