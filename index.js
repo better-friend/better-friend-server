68b554554ea960e51badc845106f081fe6918286
Francis Chen <fncis.chen@gmail.com>
2019-04-14 22:53:53 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const server = express();
 
+const datesRouter = require("./routers/datesRouter.js");
+const usersRouter = require("./routers/usersRouter.js");
+
 server.use(express.json());
+
+server.use("/api/dates", datesRouter);
+server.use("/api/users", usersRouter);
 
 server.listen(5000, () => {
     console.log("We are in the server!");
