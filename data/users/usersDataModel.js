292d17f6b01034007d21fea2ca7eb2ee17a2dc33
Francis Chen <fncis.chen@gmail.com>
2019-04-15 15:35:40 -0700

    set up user data model and routers

--- data/users/usersDataModel.js
+++ data/users/usersDataModel.js
@@ -11,5 +11,19 @@ function register(userData) {
 }
 
 function login(username, password) {
-    return db("users").where({"username": username, "password": password});
+    return db("users").where({username}).first().then(
+        user => {
+    
+          if(user && bcrypt.compareSync(password,user.password) )  {
+            let token = generateToken(user);
+    
+            // localStorage.setItem(userToken, token); 
+            console.log("You've logged in!");
+            res.status(200).send(token);
+          }
+          else {
+            res.status(401).json({errorMessage: "Incorrect password entered. Pleae enter a different password"})
+          }
+        }  
+      )
 }
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const router = express();
 
+const bcrypt = require("bcryptjs");
+const db = require("../data/users/usersDataModel");
+
+
 const authenticate = require("../middleware/authenticationMiddleware.js");
 
 router.use(express.json());
 
-router.post("/register",(req,res) => {
+router.post("/register", (req,res) => {
+    let {username, password} = req.body; 
+    let hashedPassword = bcrypt.hashSync(password, 32);
 
+    if(!username || !hashedPassword){
+        return res.status(404).json({errorMessage: "You are missing a username or password!"})
+    }
+
+    else {
+        db.register({username, hashedPassword})
+            .then(() => res.status(202).json({message: "Successful register!"}))
+            .catch(() => res.status(500).json({message: "There was a server error to accessing our database"}));
+    }
 })
 
-router.get("/login", authenticate, (req,res)=> {
+router.get("/login", (req,res) => {
 
+    let {username, password} = req.body; 
+
+    bcrypt.compareSync()
+
+    db.login({username, hashedPassword})
+        .then(() => res.status(202).json({message: "Successful register!"}))
+        .catch(() => res.status(500).json({message: "There was a server error to accessing our database"}));
 })
 
 module.exports = router;
