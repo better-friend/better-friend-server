b20c328c4f4a40799ce582f3a2336817a2e382a4
Francis Chen <fncis.chen@gmail.com>
2019-04-16 14:28:53 -0700

    added http for server

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require("express");
 const server = express();
 var cors = require('cors')
 
+const http = require('http');
 
 const datesRouter = require("./routers/datesRouter.js");
 const usersRouter = require("./routers/usersRouter.js");
 
+const app = http.createServer((req, res) => {
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+
+});
+
 server.use(express.json());
 server.use(cors());
+
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
 
 server.use("/dates", datesRouter);
 server.use("/users", usersRouter);
