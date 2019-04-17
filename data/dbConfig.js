1438a2913fbd8db4f6ebc28f6b2fea9c783eba71
Francis Chen <fncis.chen@gmail.com>
2019-04-17 09:40:04 -0700

    updated knexfile for production mode and knexconfig to chekc produciton or development mode.

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,7 @@
 const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 
-module.exports = knex(knexConfig.development);
+/// make a note to review
+const config = process.env.NODE_ENV === `production` ? knexConfig.production : knexConfig.development;
+
+module.exports = knex(config);
