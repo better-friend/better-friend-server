6f420da85f1536a7ee60fca291946ec244af60a6
Francis Chen <fncis.chen@gmail.com>
2019-04-18 22:26:04 -0700

    set up notes & outline for response testing

--- index.spec.js
+++ index.spec.js
@@ -5,11 +5,28 @@ describe("users", () => {
     
     it("should be reigstering a user", () => {
         const response = request(server).post("/users/register");
+    
+        response.send({username:  "john123", password: "blahblahblah")
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done)
+        .end(function(err, res) {
+            if (err) return done(err);
+            done();
+          });
     })
 
     it("should be logging in", () => {
         const response = request(server).post("/users/login");
 
+        response.send({username:  "john123", password: "blahblahblah")
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done)
+        .end(function(err, res) {
+            if (err) return done(err);
+            done();
+          });
     })
 
 })
@@ -19,14 +36,47 @@ describe("dates", () => {
     
     it("should be getting all dates", () => {
         const response = request(server).get("/dates/:user_id");
+
+        response.
+        .auth({username:  "john123", password: "blahblahblah"}).
+        send()
     }
     
     it("should post a message", () => {
         const response = request(server).post("/dates/:user_id");
+        
+        response.
+        .auth({username:  "john123", password: "blahblahblah"})
+        .send({
+
+
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done)
+        .end(function(err, res) {
+            if (err) return done(err);
+            done();
+          });
+    
     }
 
     it("should update a message", () => {
         const response = request(server).put("/dates/:user_id/:date_id");
+
+        response.
+        .auth({username:  "john123", password: "blahblahblah"})
+        .send({
+
+
+        })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done)
+        .end(function(err, res) {
+            if (err) return done(err);
+            done();
+          });
     }
 
     it("should delete a message", () => {
